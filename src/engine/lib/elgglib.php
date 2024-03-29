c86917506e6623b4117f95e10e642b137416a764
sandi negara <angelitasalsabilaaf@gmail.com>
2024-03-29 18:32:10 +0700

    try again

--- src/engine/lib/elgglib.php
+++ src/engine/lib/elgglib.php
@@ -1502,7 +1502,8 @@ function elgg_normalise_plural_options_array($options, $singulars) {
  * @return string The URL
  */
 function full_url() {
-	$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+#	$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+	$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on" ? "s" : "");
 	$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
 	$port = ($_SERVER["SERVER_PORT"] == "80" || $_SERVER["SERVER_PORT"] == "443") ? "" : (":".$_SERVER["SERVER_PORT"]);
 
